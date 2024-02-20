74bc4d47ca442b43f4bffd3bab70ec7824e40421
Siddharth Dash <dash.sid@northeastern.edu>
2024-02-20 04:01:56 -0500

    added json to gitignore, changed shell script location, added sample shell script in it

---
+++ packer/scripts/install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is a sample shell script for installing dependencies on CentOS Stream 8
+
+# Update package repository
+sudo yum update -y
+
+# Install some example dependencies
+sudo yum install -y curl wget unzip

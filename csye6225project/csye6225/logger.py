f23aed8c3a91d661986deb014379ad59cb741cea
Siddharth Dash <dash.sid@northeastern.edu>
2024-03-20 16:14:36 -0400

    Logger severity class override

--- csye6225project/csye6225/logger.py
+++ csye6225project/csye6225/logger.py
@@ -7,13 +7,27 @@ from pythonjsonlogger import jsonlogger
 # Defining a logger which has a file handler (to write to files) and console handler (to write to the terminal)
 # the formatter formats the log output to json
 
+# source for class overwrite : https://engineering.ziffmedia.com/formatting-python-logs-for-stackdriver-5a5ddd80761c
+# override the JsonFormatter::process_log_record function to grab the levelname attribute of log_record
+# and save it as severity
+
+class StackdriverJsonFormatter(jsonlogger.JsonFormatter, object):
+    def __init__(self, fmt="%(levelname)s %(message)", style='%', *args, **kwargs):
+        super(StackdriverJsonFormatter, self).__init__(fmt=fmt, *args, **kwargs)
+
+    def process_log_record(self, log_record):
+        log_record['severity'] = log_record['levelname']
+        del log_record['levelname']
+        return super(StackdriverJsonFormatter, self).process_log_record(log_record)
+
+
 def configure_logging():
     # Create logger
     webapp_logger = logging.getLogger(__name__)
     webapp_logger.setLevel(logging.INFO)
 
     # Configure JSON formatter
-    formatter = jsonlogger.JsonFormatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%dT%H:%M:%S%z')
+    formatter = StackdriverJsonFormatter(datefmt='%Y-%m-%dT%H:%M:%S%z')
 
     # Configure console handler
     console_handler = logging.StreamHandler()
@@ -28,7 +42,7 @@ def configure_logging():
     if os.getenv('GITHUB_ACTIONS') == 'true' or hostname.endswith('.local'):
         log_file_path = 'myapp_local.log'
     else:
-        log_file_path = '/var/log/webapp/webapp.log'
+        log_file_path = '/var/log/webapp.log'
 
     # Configure file handler
     file_handler = logging.FileHandler(log_file_path)

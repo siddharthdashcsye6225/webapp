87daaf0bec3cf9863358ac337c9ffecb2e897ebf
Siddharth Dash <dash.sid@northeastern.edu>
2024-03-26 17:35:07 -0400

    Added db for verification

--- csye6225project/csye6225/models.py
+++ csye6225project/csye6225/models.py
@@ -1,6 +1,6 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, func
-from sqlalchemy.sql.sqltypes import TIMESTAMP, UUID
+from sqlalchemy.sql.sqltypes import TIMESTAMP, UUID, Boolean
 from sqlalchemy.sql.expression import text
 
 
@@ -21,3 +21,16 @@ class User(Base):
 
     def __repr__(self):
         return f"<User(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, username={self.username})>"
+
+
+class Verification(Base):
+    __tablename__ = "verification"
+
+    id = Column(String, primary_key=True, index=True)
+    email = Column(String, nullable=False)
+    verified = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'), onupdate=func.now())
+
+    def __repr__(self):
+        return f"<Verification(id={self.id}, email={self.email}, verified={self.verified}, created_at={self.created_at})>"
